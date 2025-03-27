bbabe189a60eb48d9a632624cae51d069df09745
Matias Seltzer <107654917+MattSeltz@users.noreply.github.com>
2025-03-27 15:20:56 -0300

    Se añadieron configs

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,11 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
 
-export function middleware(req: NextRequest) {
-	console.log("Middleware cookies:", req.cookies.getAll());
-	return NextResponse.next();
+export function middleware() {
+	const res = NextResponse.next();
+	res.headers.set(
+		"Access-Control-Allow-Origin",
+		"https://xyz-servidor.onrender.com/"
+	);
+	res.headers.set("Access-Control-Allow-Credentials", "true");
+	return res;
 }
